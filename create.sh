ccaffdfcda7db04862bb3d5881dd82639b1572bd
Markus Linnala <Markus.Linnala@cybercom.com>
2021-04-14 15:14:09 +0300

    hop

--- create.sh
+++ create.sh
@@ -14,6 +14,6 @@ else
 fi
 
 make
-sudo /usr/sbin/semodule -i *.pp
+sudo /usr/sbin/semodule -i -v *.pp
 # sudo find  /etc/systemd/user /etc/xdg/systemd/user /run/systemd/user /run/user /usr/local/lib/systemd /usr/local/share/systemd /usr/share/systemd /var/lib/flatpak/exports/share/systemd /home/"$USER"/{\.config/systemd,\.local/share/systemd,\.local/share/flatpak/exports/share/systemd} -maxdepth 0 -type d -exec restorecon -vRF {} +
 exit $?
