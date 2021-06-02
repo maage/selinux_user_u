4acddca85584fcb9b203927e0c5fca4a68acb2e2
Markus Linnala <Markus.Linnala@cybercom.com>
2021-06-02 14:30:14 +0300

    hop

--- is-regression-fixed.sh
+++ is-regression-fixed.sh
@@ -2,16 +2,34 @@
 
 set -epu -o pipefail
 
-src="$1"
-tgt="$2"
-shift 2
+src=
+tgt=
+cls=
+perm=
+perms=()
+typ="allow"
+
+while (( $# )); do
+	case "$1" in
+		--allow|--auditallow|--dontaudit|--neverallow|--allowxperm|--auditallowxperm|--dontauditxperm|--neverallowxperm) typ="${1#--}"; shift ;;
+		-s|--source) (( $# >= 2 )) || exit 1; src="$2"; shift 2 ;;
+		-t|--target) (( $# >= 2 )) || exit 1; tgt="$2"; shift 2 ;;
+		-c|--class) (( $# >= 2 )) || exit 1; cls="$2"; shift 2 ;;
+		-p|--perm) (( $# >= 2 )) || exit 1; perm="$2"; shift 2 ;;
+	esac
+done
+
+if [ "$perm" ]; then
+	IFS=, read -r -a perms <<< "$perm"
+fi
+
+#
+#
+#
 
 rx_escape() {
 	sed 's/[^^a-zA-Z0-9_]/[&]/g; s/\^/\\^/g' <<< "$1"
 }
-
-src_rx+="$(rx_escape "$src")"
-tgt_rx+="$(rx_escape "$tgt")"
 
 get_typeattributeset() {
 	declare -n attrs="$1"; shift
@@ -26,21 +44,102 @@ get_typeattributeset() {
 }
 
 src_attrs=()
-get_typeattributeset src_attrs "$src_rx"
-tgt_attrs=()
-get_typeattributeset tgt_attrs "$tgt_rx"
+if [ "$src" ]; then
+	src_rx="$(rx_escape "$src")"
+	get_typeattributeset src_attrs "$src_rx"
+fi
 
-rx="[(]allow ("
-for a in "$src" "${src_attrs[@]}"; do
-	rx+="${a}|"
-done
+tgt_attrs=()
+if [ "$tgt" ]; then
+	tgt_rx="$(rx_escape "$tgt")"
+	get_typeattributeset tgt_attrs "$tgt_rx"
+fi
+
+rx="[(]$(rx_escape "$typ") ("
+
+if [ "$src" ]; then
+	for a in "$src" "${src_attrs[@]}"; do
+		rx+="${a}|"
+	done
+else
+	rx+="[^ ]*|"
+fi
+
 rx="${rx%|}) ("
-for a in "$tgt" "${tgt_attrs[@]}"; do
-	rx+="${a}|"
-done
-rx="${rx%|}) .*[( ]("
-for a in "$@"; do
-	rx+="$(rx_escape "$a")|"
-done
-rx="${rx%|})[( ]"
+
+if [ "$tgt" ]; then
+	for a in "$tgt" "${tgt_attrs[@]}"; do
+		rx+="${a}|"
+	done
+else
+	rx+="[^ ]*|"
+fi
+
+rx="${rx%|}) [(]"
+
+if [ "$cls" ]; then
+	rx+="$(rx_escape "$cls") "
+else
+	rx+="[^ ]* "
+fi
+
+perm_helper() {
+	sed -r '
+/(.).*\1/d;
+s/A/0,/;
+s/B/1,/;
+s/C/2,/;
+s/D/3,/;
+s/E/4,/;
+s/F/5,/;
+s/,$//;
+'
+}
+
+if (( ${#perms[@]} )); then
+	if (( ${#perms[@]} == 1 )); then
+		ax=(A)
+	elif (( ${#perms[@]} == 2 )); then
+		ax=({A,B}{A,B})
+	elif (( ${#perms[@]} == 3 )); then
+		ax=({A,B,C}{A,B,C}{A,B,C})
+	elif (( ${#perms[@]} == 4 )); then
+		ax=({A,B,C,D}{A,B,C,D}{A,B,C,D}{A,B,C,D})
+	elif (( ${#perms[@]} == 5 )); then
+		ax=({A,B,C,D,E}{A,B,C,D,E}{A,B,C,D,E}{A,B,C,D,E}{A,B,C,D,E})
+	elif (( ${#perms[@]} == 6 )); then
+		ax=({A,B,C,D,E,F}{A,B,C,D,E,F}{A,B,C,D,E,F}{A,B,C,D,E,F}{A,B,C,D,E,F}{A,B,C,D,E,F})
+	else
+		echo "ERROR: too many to permute, implement it"
+		exit 1
+	fi
+
+	rx+="[(]("
+	while read -r permutation; do
+		IFS=, read -r -a idx_arr <<< "$permutation"
+		rx+="[^()]* )?"
+		flag=0
+		for idx in "${idx_arr[@]}"; do
+			if (( flag )); then
+				rx+="[^()]* "
+			else
+				flag=1
+			fi
+			rx+="$(rx_escape "${perms[$idx]}")"
+		done
+		rx+="[^()]* )?"
+		rx+="|"
+	done < <(
+		for a in "${ax[@]}"; do 
+			printf "%s\n" "$a"
+		done | perm_helper
+	)
+
+	rx="${rx%|})[)]"
+else
+	rx+="[(][^)]*[)]"
+fi
+rx+="[)]"
+
+set -x
 grep -Er "$rx" export
