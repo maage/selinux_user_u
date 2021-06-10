49f898658fc544b51a3dd21fb137ba19227b0c2b
Markus Linnala <Markus.Linnala@cybercom.com>
2021-06-10 19:15:10 +0300

    hop

--- te_lines.sh
+++ te_lines.sh
@@ -8,7 +8,7 @@ for a in *.te;do
 	cnt=0
 	requires="$(sed -n '/^[[:space:]]*require {/,/^[[:space:]]*}/{/^[[:space:]]*require {/d;/^[[:space:]]*}/d;/^[[:space:]]*#/d;s/^[[:space:]]*//;p}' "$a"|sort -u)"
 	while read -r line; do
-		printf "policy_module(%s_%d, 1.0.0)\nrequires {\n%s\n}\n%s\n" "${a%.te}" $cnt "$requires" "$line" > module_lines/"${a%.te}"_"$cnt".te
+		printf "policy_module(%s_%d, 1.0.0)\nrequire {\n%s\n}\n%s\n" "${a%.te}" $cnt "$requires" "$line" > module_lines/"${a%.te}"_"$cnt".te
 		(( cnt++ )) || :
-	done < <(sed -n '/^[[:space:]]*require {/,/^[[:space:]]*}/d;/^[[:space:]]*#/d;/^$/d;/^tunable_policy/d;/^optional_policy/d;/^'"'"')/d;p' "$a")
+	done < <(sed -n '/^policy_module(/d;/^[[:space:]]*require {/,/^[[:space:]]*}/d;/^[[:space:]]*#/d;/^$/d;/^tunable_policy/d;/^optional_policy/d;/^'"'"')/d;p' "$a")
 done
