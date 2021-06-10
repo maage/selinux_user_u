e9b9e05283da1467455d1e22b2cf4ea0e9b10265
Markus Linnala <Markus.Linnala@cybercom.com>
2021-06-10 19:13:58 +0300

    hop

---
+++ te_lines.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -epux -o pipefail
+
+mkdir -p module_lines
+
+for a in *.te;do
+	cnt=0
+	requires="$(sed -n '/^[[:space:]]*require {/,/^[[:space:]]*}/{/^[[:space:]]*require {/d;/^[[:space:]]*}/d;/^[[:space:]]*#/d;s/^[[:space:]]*//;p}' "$a"|sort -u)"
+	while read -r line; do
+		printf "policy_module(%s_%d, 1.0.0)\nrequires {\n%s\n}\n%s\n" "${a%.te}" $cnt "$requires" "$line" > module_lines/"${a%.te}"_"$cnt".te
+		(( cnt++ )) || :
+	done < <(sed -n '/^[[:space:]]*require {/,/^[[:space:]]*}/d;/^[[:space:]]*#/d;/^$/d;/^tunable_policy/d;/^optional_policy/d;/^'"'"')/d;p' "$a")
+done
