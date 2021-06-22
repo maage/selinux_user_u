c33fc58145d2efbd7a177b5de62eb311494e910f
Markus Linnala <Markus.Linnala@cybercom.com>
2021-06-22 15:41:07 +0300

    get.sh: extend rules

--- get.sh
+++ get.sh
@@ -4,13 +4,18 @@ myfilter() {
 	sed -r '
 /  denied /!d;
 / (siginh|rlimitinh|noatsecure) /d;
-/comm="(ps|pgrep)".*dev="proc"/d;
-/\{ sys_ptrace \} .* comm="(chrome|ps|systemd-journal|MemoryInfra)" .* tclass=cap_userns /d;
+/ comm="(ps|pgrep)" /{/ dev="proc" /d};
+/ \{ sys_ptrace \} /{/ comm="(chrome|ps|systemd-journal|MemoryInfra)" /{/ tclass=cap_userns /d}};
+/ comm="less" /{/ scontext=[^:]*:[^:]*:journalctl_t:/{
+	/ \{ getattr \} /{/ path="[/]dev[/][^"]*" dev="(devtmpfs|devpts|hugetlbfs)" /d};
+	/ \{ getattr \} /{/ tcontext=system_u:object_r:(initctl_t|proc_kcore_t):/d};
+	/ \{ search \} /{/ name="[/]" dev="devpts" /d};
+}}
 	'
 }
 
 mysource() {
-	journalctl --since -5m SYSLOG_IDENTIFIER=audit
+	journalctl --since -30m SYSLOG_IDENTIFIER=audit
 }
 
 #journalctl --since -2h SYSLOG_IDENTIFIER=audit|sed -r '/  denied /!d;/comm="(gnome-terminal-|cpan|perl|dnf)"/d'|audit2allow -r -R|egrep -v '^#|^$'
