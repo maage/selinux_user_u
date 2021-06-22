486f82bb4c7c115786d86602a2ad10bcd60e6432
Markus Linnala <Markus.Linnala@cybercom.com>
2021-06-22 15:04:17 +0300

    get.sh: group common

--- get.sh
+++ get.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 set -x
+myfilter() {
+	sed -r '
+/  denied /!d;
+/ (siginh|rlimitinh|noatsecure) /d;
+/comm="(ps|pgrep)".*dev="proc"/d;
+/\{ sys_ptrace \} .* comm="(chrome|ps|systemd-journal|MemoryInfra)" .* tclass=cap_userns /d;
+	'
+}
+
+mysource() {
+	journalctl --since -5m SYSLOG_IDENTIFIER=audit
+}
+
 #journalctl --since -2h SYSLOG_IDENTIFIER=audit|sed -r '/  denied /!d;/comm="(gnome-terminal-|cpan|perl|dnf)"/d'|audit2allow -r -R|egrep -v '^#|^$'
 if (($#)); then
-	journalctl --since -5m SYSLOG_IDENTIFIER=audit|sed -r '/  denied /!d;/ (siginh|rlimitinh|noatsecure) /d;/comm="ps".*dev="proc"/d' |audit2allow -r "$@"
+	mysource | myfilter  | audit2allow -r "$@"
 	exit $?
 fi
-journalctl --since -5m SYSLOG_IDENTIFIER=audit|sed -r '/  denied /!d;/ (siginh|rlimitinh|noatsecure) /d;/comm="ps".*dev="proc"/d' |audit2allow -r|a2a-cleanup
+mysource | myfilter | audit2allow -r | a2a-cleanup
