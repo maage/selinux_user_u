607fc20aa6dab9b7c28fab1b94c2f6dce2d1c7d7
Markus Linnala <Markus.Linnala@cybercom.com>
2021-06-22 14:40:48 +0300

    create.sh: simplify

--- create.sh
+++ create.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 
 set -epux -o pipefail
-rc=0
-git status --porcelain | grep -Eq '^' || rc=$?
-if [ $rc -eq 0 ]; then
-        git add -u
-        git commit -m hop
-elif [ $rc -eq 1 ]; then
-        :
-else
-        echo "RC=$?"
-        exit $?
-fi
-
-make -f /usr/share/selinux/devel/Makefile local_user_u.pp
-sudo /usr/sbin/semodule -i local_user_u.pp
+make
 sudo find  /etc/systemd/user /etc/xdg/systemd/user /run/systemd/user /run/user /usr/local/lib/systemd /usr/local/share/systemd /usr/share/systemd /var/lib/flatpak/exports/share/systemd /home/"$USER"/{\.config/systemd,\.local/share/systemd,\.local/share/flatpak/exports/share/systemd} -maxdepth 0 -type d -exec restorecon -vRF {} +
 exit $?
