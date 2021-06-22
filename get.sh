7b59b31036e00d949e1f7acf46aecda9ade37c87
Markus Linnala <Markus.Linnala@cybercom.com>
2021-06-22 16:04:48 +0300

    get.sh: tune time

--- get.sh
+++ get.sh
@@ -15,7 +15,7 @@ myfilter() {
 }
 
 mysource() {
-	journalctl --since -30m SYSLOG_IDENTIFIER=audit
+	journalctl --since -5m SYSLOG_IDENTIFIER=audit
 }
 
 #journalctl --since -2h SYSLOG_IDENTIFIER=audit|sed -r '/  denied /!d;/comm="(gnome-terminal-|cpan|perl|dnf)"/d'|audit2allow -r -R|egrep -v '^#|^$'
